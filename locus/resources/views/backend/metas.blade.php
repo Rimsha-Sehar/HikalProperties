d942ffa0cfe144612066b37d6a4e2d4b75ba3856
unknown <usamaramzan978@gmail.com>
2023-10-25 18:20:45 +0500

    Hikal Properties Pushed

---
+++ locus/resources/views/backend/metas.blade.php
@@ -0,0 +1,5 @@
+<!-- all the meta tags -->
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta content="" name="description" />
+<meta content="" name="author" />
