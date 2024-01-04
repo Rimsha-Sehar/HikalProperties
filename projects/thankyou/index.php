1f82cb5aea6feed4ce765309db7389778acb5bf8
jimuskanin <workofmuskan@gmail.com>
2024-01-04 16:25:45 +0400

    THANK YOU PIXELS

--- projects/thankyou/index.php
+++ projects/thankyou/index.php
@@ -80,37 +80,30 @@ if ($_SESSION['leadSource'] == "Campaign Snapchat") {
     <!-- ICON -->
     <link rel="icon" type="image/png" href="https://hikalproperties.com/projects/assets/images/logo/hikalagency-icon.png" />
     <!-- https://hikalproperties.com/projects/ -->
+    
     <!-- STYLES -->
-
     <link rel="stylesheet" href="https://hikalproperties.com/projects/assets/css/dark-theme-gold.css" />
     <link rel="stylesheet" href="https://hikalproperties.com/projects/assets/css/animation.css" />
 
-    <!-- Google Tag Manager -->
-    <!-- <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-K7J7SRR');
-    </script> -->
-    <!-- End Google Tag Manager -->
+    <!-- META PIXEL -->
+    <script src="https://hikalproperties.com/projects/gtm/meta.js"></script> 
+    <!-- TIKTOK PIXEL -->
+    <script src="https://hikalproperties.com/projects/gtm/tiktok.js"></script>
+
+    <!-- TIKTOK PIXEL  -->
+    <?php
+    if ($_SESSION['leadSource'] == "Campaign Snapchat") {
+        ?>
+        <script>
+            ttq.track('CompleteRegistration');
+        </script>
+        <?php
+    }
+    ?>
+    <!-- TIKTOK PIXEL END  -->
 </head>
 
 <body class="english">
-    <!-- Google Tag Manager (noscript) -->
-    <!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7J7SRR" height="0" width="0" style="display:none; visibility:hidden"></iframe></noscript> -->
-    <!-- End Google Tag Manager (noscript) -->
-
-    
     <div class="d-flex flex-column justify-content-between" style="width: 100%; min-height: 100vh;">
         <?php
             include_once("../components/header-dark.php");
