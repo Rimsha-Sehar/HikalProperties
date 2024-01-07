2f0e6ee50fb7c76408bcf9beb21b394c14abd9b4
jimuskanin <workofmuskan@gmail.com>
2024-01-07 11:20:52 +0400

    TWITTER PIXEL TEST

--- projects/cbd/en-x/index.php
+++ projects/cbd/en-x/index.php
@@ -100,7 +100,13 @@ curl_close($ch);
     <!-- TIKTOK PIXEL -->
     <script src="https://hikalproperties.com/projects/gtm/tiktok.js"></script>
     <!-- TWITTER PIXEL -->
-    <script src="https://hikalproperties.com/projects/gtm/twitter.js"></script>
+    <!-- <script src="https://hikalproperties.com/projects/gtm/twitter.js"></script> -->
+    <script>
+        !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','ohu9a');
+    </script>
 </head>
 
 <body class="english">
