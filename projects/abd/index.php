996d785b196d9e692b8f743ac3d500d3e2198733
Usama Ramzan <76843134+usamaramzan978@users.noreply.github.com>
2024-01-19 17:59:02 +0500

    Update index.php==projects/index.php tags are added in head and body section

--- projects/abd/index.php
+++ projects/abd/index.php
@@ -97,6 +97,22 @@ curl_close($ch);
 
     <!-- PIXEL -->
     <script src="https://hikalproperties.com/projects/gtm/pixel.js"></script> 
+// Google Tag Manager
+(function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-K7J7SRR');
+// End Google Tag Manager
 
     <style>
         .gold-text {
@@ -184,6 +200,15 @@ curl_close($ch);
 </head>
 
 <body class="arabic" dir="rtl">
+    <!-- GTM - JAN 19, 2024 -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMK8LXSJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+
+<!-- META - BODY -->
+<noscript>
+	<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=979060579385097&ev=PageView&noscript=1" />
+</noscript>
     <?php include_once("../gtm/pixel.php"); ?>
     <?php
     $checkip = mysqli_query($con, "SELECT byIP FROM is_blocked WHERE status = 1 AND byIP = '$ip'");
