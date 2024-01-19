e98d363a4c922f36477f7b41f4a636e190005cdb
Usama Ramzan <76843134+usamaramzan978@users.noreply.github.com>
2024-01-19 16:47:27 +0500

    Update include_top.blade.php==tags addded

--- resources/views/global/include_top.blade.php
+++ resources/views/global/include_top.blade.php
@@ -48,14 +48,6 @@
     <link rel="stylesheet" href="{{ asset('public/assets/global/css/mapbox-gl-geocoder.css') }}">
     <script src="{{ asset('public/assets/global/js/mapbox-gl.js') }}"></script>
     <script src="{{ asset('public/assets/global/js/mapbox-gl-geocoder.min.js') }}"></script>
-<script>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WMK8LXSJ');</script>
-<!-- End Google Tag Manager -->
-</script>
+
 
 
