48d0fd0aad8f23112c5ff07fd5bc858c327973e7
jimuskanin <workofmuskan@gmail.com>
2024-03-15 09:05:10 +0400

    PRIVACY POLICY LINKED

--- .history/projects/components/footer-light_20240314114651.php
+++ .history/projects/components/footer-light_20240314114651.php
@@ -10,8 +10,8 @@
             <a class="mx-2" href="https://www.google.com/maps/place/Hikal+Agency/@25.22559,55.2832632,15z/data=!4m5!3m4!1s0x0:0x147f0ab670b91e1d!8m2!3d25.22559!4d55.2832632" target="_blank"><i class="fa fa-map-marker" style="color: #DA1F26;"></i></a>
         </div>
     </div>
-</div>
+</div> 
 <div class="bg-black text-white py-3 w-full text-center">
     Copyright &copy; <?php echo date('Y'); ?>
-    <strong><a href="https://hikalproperties.com/" style="color: #DA1F26;">HIKAL</a></strong>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="https://hikalproperties.com/privacypolicy" style="color: #fff;">Privacy Policy</a>
+    <strong><a href="https://hikalproperties.com/" style="color: #DA1F26;">HIKAL</a></strong>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="privacypolicy" style="color: #fff;">Privacy Policy</a>
 </div>
