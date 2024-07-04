8475539f445ddff70fd216b780e78583bba8147c
jimuskanin <workofmuskan@gmail.com>
2024-07-04 15:53:43 +0400

    BINGHATTI HILLS

---
+++ .history/projects/binghatti/hills/ar-location_20240704154217.php
@@ -0,0 +1,79 @@
+<div class="third_section container container-fluid mt-5 py-5">
+    <h4 class="primary-text text-center mb-5">
+        مميزات الموقع
+    </h4>
+    <div class="row container container-fluid">
+        <!-- DUBAI HILLS MALL  -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="../../../assets/images/projects/binghatti/hills/mall.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    دبي هيلز مول
+                </div>
+            </div>
+        </div>
+        <!-- PALM JUMEIRAH -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="../../../assets/images/projects/binghatti/hills/palmjumeirah.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    نخلة جميرا
+                </div>
+            </div>
+        </div>
+        <!-- BURJ AL ARAB -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="../../../assets/images/projects/binghatti/hills/burjalarab.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    برج العرب
+                </div>
+            </div>
+        </div>
+        <!-- BUTTERFLY GARDEN -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="../../../assets/images/projects/binghatti/hills/butterfly.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    حديقة الفراشات
+                </div>
+            </div>
+        </div>
+        <!-- MIRACLE GARDEN -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="../../../assets/images/projects/binghatti/hills/flower.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    حديقة معجزة
+                </div>
+            </div>
+        </div>
+        <!-- GLOBAL VILLAGE -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="../../../assets/images/projects/binghatti/hills/ferris.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    قرية عالمية
+                </div>
+            </div>
+        </div>
+        <!-- JUMEIRAH GOLF ESTATES -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="../../../assets/images/projects/binghatti/hills/golf.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    عقارات جميرا للجولف
+                </div>
+            </div>
+        </div>
+        <!-- IMG WORLDS OF ADVENTURE -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="../../../assets/images/projects/binghatti/hills/rollercoaster.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    آي إم جي عالم من المغامرات
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ projects/binghatti/hills/en-location.php
@@ -0,0 +1,79 @@
+<div class="third_section container container-fluid mt-5 py-5">
+    <h4 class="primary-text text-center mb-5">
+        LOCATION BENEFITS
+    </h4>
+    <div class="row container container-fluid">
+        <!-- DUBAI HILLS MALL  -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="https:.//hikalproperties.com/projects/assets/images/projects/binghatti/hills/mall.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    DUBAI HILLS MALL
+                </div>
+            </div>
+        </div>
+        <!-- PALM JUMEIRAH -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="https:.//hikalproperties.com/projects/assets/images/projects/binghatti/hills/palmjumeirah.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    PALM JUMEIRAH
+                </div>
+            </div>
+        </div>
+        <!-- BURJ AL ARAB -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="https:.//hikalproperties.com/projects/assets/images/projects/binghatti/hills/burjalarab.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    BURJ AL ARAB
+                </div>
+            </div>
+        </div>
+        <!-- BUTTERFLY GARDEN -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="https:.//hikalproperties.com/projects/assets/images/projects/binghatti/hills/butterfly.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    BUTTERFLY GARDEN
+                </div>
+            </div>
+        </div>
+        <!-- MIRACLE GARDEN -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="https:.//hikalproperties.com/projects/assets/images/projects/binghatti/hills/flower.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    MIRACLE GARDEN
+                </div>
+            </div>
+        </div>
+        <!-- GLOBAL VILLAGE -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="https:.//hikalproperties.com/projects/assets/images/projects/binghatti/hills/ferris.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    GLOBAL VILLAGE
+                </div>
+            </div>
+        </div>
+        <!-- JUMEIRAH GOLF ESTATES -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="https:.//hikalproperties.com/projects/assets/images/projects/binghatti/hills/golf.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    JUMEIRAH GOLF ESTATES
+                </div>
+            </div>
+        </div>
+        <!-- IMG WORLDS OF ADVENTURE -->
+        <div class="col-6 col-md-4 col-lg-3 pb-4">
+            <div class="d-flex flex-column justify-content-start align-items-center">
+                <img src="https:.//hikalproperties.com/projects/assets/images/projects/binghatti/hills/rollercoaster.svg" width="40" />
+                <div class="location-name my-2 text-black">
+                    IMG WORLDS OF ADVENTURE
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
