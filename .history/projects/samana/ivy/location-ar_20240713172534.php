7edce85148ddfe6c2c80b0db202e43c9ae4e9ee1
jimuskanin <workofmuskan@gmail.com>
2024-07-13 17:31:50 +0400

    SAMANA IVY

---
+++ .history/projects/samana/ivy/location-ar_20240713172534.php
@@ -0,0 +1,56 @@
+<?php
+?>
+<!-- LOCATION BENEFITS -->
+<div class="third_section container container-fluid my-4 py-4">
+    <h4 class="primary-text text-center">
+        مميزات الموقع
+    </h4>
+    <div class="row container container-fluid d-flex justify-content-center">
+        <div class="col-12 col-md-6 w-full p-4 d-flex flex-column justify-content-center align-items-center">
+            <h6 class="my-0 mb-2" style="font-weight: bold;" dir="ltr">
+                10 - 15 MIN
+            </h6>
+            <!-- GLOBAL VILLAGE -->
+            <h6 class="m-0">
+                القارية العالمية
+            </h6>
+            <!-- DUBAI OUTLET MALL -->
+            <h6 class="m-0">
+                دبي أوتلت مول
+            </h6>
+            <!-- IMG WORLD -->
+            <h6 class="m-0">
+                مغامرة آي إم جي
+            </h6>
+            <!-- FAIRGREEN INTERNATIONAL SCHOOL -->
+            <h6 class="m-0">
+                مدرسة فيرجرين الدولية
+            </h6>
+        </div>
+        <div class="col-12 col-md-6 w-full p-4 d-flex flex-column justify-content-center align-items-center">
+            <h6 class="my-0 mb-2" style="font-weight: bold;" dir="ltr">
+                15 - 20 MIN
+            </h6>
+            <!-- BURJ KHALIFA AND DUBAI MALL -->
+            <h6 class="m-0">
+                برج خليفة و دبى مول
+            </h6>
+            <!-- DUBAI DOWNTOWN -->
+            <h6 class="m-0">
+                وسط مدينة دبى
+            </h6>
+            <!-- DUBAI AIRPORT -->
+            <h6 class="m-0">
+                مطار دبي الدولي
+            </h6>
+            <!-- MIRACLE GARDEN  -->
+            <h6 class="m-0">
+                حديقة دبي للزهور العجيبة
+            </h6>
+            <!-- MIRDIF CITY CENTER -->
+            <h6 class="m-0">
+                مردف سيتي سنتر
+            </h6>
+        </div>
+    </div>
+</div>
---
+++ .history/projects/samana/ivy/location-en_20240713172453.php
@@ -0,0 +1,55 @@
+<?php
+?>
+<div class="third_section container container-fluid my-4 py-4">
+    <h4 class="primary-text text-center">
+        LOCATION BENEFITS
+    </h4>
+    <div class="row container container-fluid d-flex justify-content-center">
+        <div class="col-12 col-md-6 w-full p-4 d-flex flex-column justify-content-center align-items-center">
+            <h6 class="my-0 mb-2" style="font-weight: bold;" dir="ltr">
+                10 - 15 MIN
+            </h6>
+            <!-- GLOBAL VILLAGE -->
+            <h6 class="m-0">
+                GLOBAL VILLAGE
+            </h6>
+            <!-- DUBAI OUTLET MALL -->
+            <h6 class="m-0">
+                DUBAI OUTLET MALL
+            </h6>
+            <!-- IMG WORLD -->
+            <h6 class="m-0">
+                IMG WORLD
+            </h6>
+            <!-- FAIRGREEN INTERNATIONAL SCHOOL -->
+            <h6 class="m-0">
+                FAIRGREEN INTERNATIONAL SCHOOL
+            </h6>
+        </div>
+        <div class="col-12 col-md-6 w-full p-4 d-flex flex-column justify-content-center align-items-center">
+            <h6 class="my-0 mb-2" style="font-weight: bold;" dir="ltr">
+                15 - 20 MIN
+            </h6>
+            <!-- BURJ KHALIFA AND DUBAI MALL -->
+            <h6 class="m-0">
+                BURJ KHALIFA AND DUBAI MALL
+            </h6>
+            <!-- DUBAI DOWNTOWN -->
+            <h6 class="m-0">
+                DUBAI DOWNTOWN
+            </h6>
+            <!-- DUBAI AIRPORT -->
+            <h6 class="m-0">
+                DUBAI AIRPORT
+            </h6>
+            <!-- MIRACLE GARDEN  -->
+            <h6 class="m-0">
+                MIRACLE GARDEN
+            </h6>
+            <!-- MIRDIF CITY CENTER -->
+            <h6 class="m-0">
+                MIRDIF CITY CENTER
+            </h6>
+        </div>
+    </div>
+</div>
