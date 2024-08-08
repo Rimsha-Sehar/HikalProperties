fa00e635015e3859b9bfe72b0f528fa9090541d7
Muhammad Danish <worldac2017@gmail.com>
2024-08-08 11:16:52 +0500

    update api's

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\API\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Auth controllers
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
